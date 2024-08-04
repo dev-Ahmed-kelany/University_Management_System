8958fecff27deac59c6e1045e844dd46c71bf235
Ahmed <dev.ahmed.kelany@gmail.com>
2024-08-04 13:17:06 +0300

    Initialize projects

---
+++ University_Management_System_Business/Dashboard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data;
+
+using University_Management_System_DataAccess;
+
+namespace University_Management_System_Business
+{
+    public class clsDashboard
+    {
+
+    }
+}
---
+++ University_Management_System_DataAccess/DataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Configuration;
+
+namespace University_Management_System_DataAccess
+{
+    public class clsDataAccessSettings
+    {
+        public static string ConnectionString = ConfigurationManager
+            .ConnectionStrings["University_Managment_System_Connection"].ConnectionString;
+    }
+}
